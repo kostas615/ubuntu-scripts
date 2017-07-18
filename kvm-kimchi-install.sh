c079db09bc6e5d45a39a090e4cc146c9720c70f7
kostas615 <kostaschatzis01@gmail.com>
2017-07-18 20:03:52 +0300

    kvm-kimchi

--- kvm-kimchi-install.sh
+++ kvm-kimchi-install.sh
@@ -10,24 +10,28 @@ apt install qemu qemu-kvm libvirt-bin
 apt install nginx
 
 # KIMCHI
-apt-get -y install python-cherrypy3 python-jsonschema python-m2crypto nginx python-ldap python-psutil fonts-font-awesome texlive-fonts-extra python-configobj python-parted sosreport python-imaging websockify novnc nfs-common python-ethtool open-iscsi python-guestfs libguestfs-tools spice-html5 python-paramiko
-wget http://kimchi-project.github.io/wok/downloads/wok-2.2.0-0.noarch.deb
-wget http://kimchi-project.github.io/gingerbase/downloads/ginger-base-2.2.0-0.noarch.deb
-wget http://kimchi-project.github.io/kimchi/downloads/kimchi-2.2.0-0.noarch.deb
+apt install python-cherrypy3 python-jsonschema python-m2crypto nginx python-ldap python-psutil fonts-font-awesome texlive-fonts-extra python-configobj python-parted sosreport python-imaging websockify novnc nfs-common python-ethtool open-iscsi python-guestfs libguestfs-tools spice-html5 python-paramiko
+wget http://kimchi-project.github.io/wok/downloads/latest/wok.noarch.deb
+wget http://kimchi-project.github.io/gingerbase/downloads/latest/ginger-base.noarch.deb
+wget http://kimchi-project.github.io/kimchi/downloads/latest/kimchi.noarch.deb 
 
 # INSTALL
 dpkg -i wok.noarch.deb 
-sudo apt-get install -f
+apt install -f
+dpkg -i wok.noarch.deb 
 dpkg -i ginger-base.noarch.deb
-sudo apt-get install -f
+apt install -f
+dpkg -i ginger-base.noarch.deb
 dpkg -i kimchi.noarch.deb
-sudo apt-get install -f
+apt install -f
+dpkg -i kimchi.noarch.deb
 
 # Reload WOK
 systemctl daemon-reload
 systemctl restart wokd
 
 # End dialog
+echo " "
 echo "*************************************"
 echo " Kimchi is on https://localhost:8001 "
 echo "*************************************"
